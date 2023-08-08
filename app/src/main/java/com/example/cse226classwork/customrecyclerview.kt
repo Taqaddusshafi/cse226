6b946f7a920bb60880398a0c5407777a02b7f734
Taqaddus <taqaddusshafi9086@gmail.com>
2023-08-08 12:47:04 +0530

    custom recycler view

---
+++ app/src/main/java/com/example/cse226classwork/customrecyclerview.kt
@@ -0,0 +1,11 @@
+package com.example.cse226classwork
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class customrecyclerview : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customrecyclerview)
+    }
+}
---
+++ app/src/main/java/com/example/cse226classwork/newdata.kt
@@ -0,0 +1,4 @@
+package com.example.cse226classwork
+
+class newdata {
+}
