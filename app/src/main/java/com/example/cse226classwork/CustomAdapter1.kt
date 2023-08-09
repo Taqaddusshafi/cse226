792d882ae3ff5972bedf3b94cbd83f080b51bdc6
Taqaddus <taqaddusshafi9086@gmail.com>
2023-08-10 00:45:45 +0530

    added onlick listner and delete button

--- app/src/main/java/com/example/cse226classwork/CustomAdapter1.kt
+++ app/src/main/java/com/example/cse226classwork/CustomAdapter1.kt
@@ -1,44 +1,48 @@
 package com.example.cse226classwork
-
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class CustomAdapter1(private val mList: List<ItemsViewModel>) : RecyclerView.Adapter<CustomAdapter1.ViewHolder>() {
-
+class CustomAdapter1(private var mList: MutableList<ItemsViewModel>) : RecyclerView.Adapter<CustomAdapter1.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.newdesignview, parent, false)
-
         return ViewHolder(view)
     }
 
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = mList[position]
 
-        val ItemsViewModel = mList[position]
+        holder.imageView.setImageResource(item.image)
+        holder.textView.text = item.text
 
-
-        holder.imageView.setImageResource(ItemsViewModel.image)
-
-
-        holder.textView.text = ItemsViewModel.text
-
+        holder.button.setOnClickListener {
+            mList.removeAt(position)
+            notifyItemRemoved(position)
+            notifyItemRangeChanged(position, mList.size)
+        }
     }
-
 
     override fun getItemCount(): Int {
         return mList.size
     }
 
-
-    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imageView: ImageView = itemView.findViewById(R.id.imageview)
         val textView: TextView = itemView.findViewById(R.id.textView)
+        val button: ImageButton = itemView.findViewById(R.id.btndlt)
+
+        init {
+            textView.setOnClickListener {
+                Toast.makeText(itemView.context, "${textView.text}", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
